3aec560bee37b7a388e1c5e3828e79faef4ea78a
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-02-05 13:21:47 +0100

    Fix layout for small devices

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,13 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 
 function Home({ scrollPosition }) {
     return (
-        <Container id="home" className="d-flex flex-row align-items-center justify-content-center">
+        <Container id="home" className="d-flex flex-column align-items-center justify-content-center py-5">
             <Row>
                 <Col>
                     <Image className={(scrollPosition === 0) && "downEffect"} src="./images/yykoca.jpg" roundedCircle width="240" height="240" />
                 </Col>
             </Row>
-            <Row className='w-50 ms-5'>
+            <Row className='p-2 mt-5 text-center'>
                 <Col xs className={(scrollPosition === 0) ? "upEffect" : ""} >
                     <span className='fs-4'>Hi, my name is</span>
                     <h1 className="mb-0 display-3">Yahya Yasir Koca.</h1>
