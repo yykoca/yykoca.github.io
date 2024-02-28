b4a636adf31f6b04345647485217bb95d1a0d96c
Yahya Yasir Koca <yykoca.08@gmail.com>
2024-02-28 13:54:35 +0100

    Add scrollToTop function for home link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Navbar, Container, Nav, Image } from 'react-bootstrap';
 
 function Header() {
+    const scrollToTop = () => {
+        window.scrollTo({top:0, behavior: 'smooth'})
+    };
+
     return (
         <Navbar id="header" collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
             <Container>
@@ -9,7 +13,7 @@ function Header() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                     <Nav>
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link onClick={scrollToTop}>Home</Nav.Link>
                         <Nav.Link href="#about">About</Nav.Link>
                         <Nav.Link href="#experience">Experience</Nav.Link>
                         <Nav.Link href="#project">Project</Nav.Link>
